d6a7a8d5a5dbcd8296597a4e3e9559c5e9f65068
Ryan Wittrup <ryan.wittrup@gmail.com>
2016-09-02 21:40:32 -0400

    Add initial tests

---
+++ test.js
@@ -0,0 +1,14 @@
+var request = require('supertest');
+var app = require('./app');
+
+describe('Requests to the rooth path', function(){
+  it('Returns a 200 status code', function(done){
+    request(app)
+      .get('/')
+      .expect(200)
+      .end(function(error){
+        if(error) throw error;
+        done();
+      });
+  });
+});
