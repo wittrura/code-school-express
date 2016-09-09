6a982f9016c6c82866a530fb829fdcb9fe97b5f3
Ryan Wittrup <ryan.wittrup@gmail.com>
2016-09-08 23:19:41 -0400

    Add redis config for heroku deploy

--- app.js
+++ app.js
@@ -6,10 +6,19 @@ var urlEncode = bodyParser.urlencoded({ extended: false });
 
 app.use(express.static('public'));
 
+// Redis Connection
 var redis = require('redis');
-var client = redis.createClient();
+// var redis = require("redis").createClient();
+if (process.env.REDISTOGO_URL) {
+  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var client = redis.createClient(rtg.port, rtg.hostname);
+  client.auth(rtg.auth.split(":")[1]);
+} else {
+  var client = redis.createClient();
+}
 
 client.select((process.env.NODE_ENV || 'development').length);
+// End Redis Connection
 
 client.hset('cities', 'Lotopia', 'Description');
 client.hset('cities', 'Caspiana', 'Description');
